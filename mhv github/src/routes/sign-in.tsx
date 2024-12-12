1b12c4be1cd9e3b0932e069c86c4caa77a30292d
jongitshub <jonbryant93@gmail.com>
2024-12-12 06:27:14 -0500

    Delete mhv github/src directory

--- mhv github/src/routes/sign-in.tsx
+++
@@ -1,7 +0,0 @@
-import { SignIn } from '@clerk/clerk-react'
-
-export default function SignInPage() {
-  return <div className='justify-self-center p-10'>
-    <SignIn fallbackRedirectUrl="/home" path="/sign-in" />
-    </div>
-}
